1f1a5bb31036df3d52e965d7efa89fde562f8226
Ricardo Barros <ricardofbarros@hotmail.com>
2015-06-21 20:26:29 +0100

    improve getImage

--- app/getImage.js
+++ app/getImage.js
@@ -2,11 +2,13 @@
 var url = require('url');
 var crawler = require('lib/crawler');
 
+// Route handler
 module.exports = function (req, res) {
   if (!req.query.domain) {
     return res.boom.badRequest('Missing query param domain');
   }
 
+  var domainString = req.query.domain;
   var type = !req.query.type ? 'favicon' : req.query.type;
 
   var possibleTypes = [
@@ -22,44 +24,33 @@ module.exports = function (req, res) {
     return res.boom.badRequest('Invalid type');
   }
 
-  // Exception - clean array
-  // because all types must be processed
-  // before answering the request
-  if (type === 'all') {
-    possibleTypes = [];
-  } else {
-    possibleTypes.splice(possibleTypes.indexOf('all'));
-    possibleTypes.splice(type);
-  }
-
-  var domain = url.parse(req.query.domain);
+  var domain = url.parse(domainString);
 
   if (!domain.protocol) {
     return res.boom.badRequest('Invalid domain');
   }
 
+  // TODO document this handler
+  // and make it more all-purpose
+  var responseHandler = function (err, href) {
+    if (err) {
+      return res.boom.badRequest(err);
+    }
+
+    var sendObj;
+
+    if (typeof href === 'string') {
+      sendObj = {};
+      sendObj[type] = href;
+    } else {
+      sendObj = href;
+    }
+
+    return res.json(sendObj);
+  };
+
   crawler.queue({
-    uri: req.query.domain,
-    callback: crawler.findIcons.bind({
-      type: type,
-      // TODO put this handle elsewhere
-      // and document it
-      callback: function (err, href) {
-        if (err) {
-          return res.boom.badRequest(err);
-        }
-
-        var sendObj;
-
-        if (type !== 'all' && typeof href === 'string') {
-          sendObj = {};
-          sendObj[type] = href;
-        } else {
-          sendObj = href;
-        }
-
-        return res.json(sendObj);
-      }
-    })
+    uri: domainString,
+    callback: crawler.findIcons(type, responseHandler)
   });
 };
