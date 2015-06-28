4e736d7faeb0ae2733b973fbb6526cec0973205e
Ricardo Barros <ricardofbarros@hotmail.com>
2015-06-28 23:23:23 +0100

    add workers to app.js

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var boom = require('express-boom');
 var checkSysDeps = require('./checkSysDeps');
 var routesHandlers = require('./app/index');
 var mkdirp = require('mkdirp');
+var workerFarm = require('worker-farm');
 
 // Create tmp and tmp/__queueMsAppIcons
 // directories
@@ -12,6 +13,10 @@ mkdirp('tmp/__queueMsAppIcons', function (err, success) {
   if (err) {
     return console.log(err || new Error('Couldn\'t create tmp and tmp/__queueMsAppIcons directories'));
   }
+
+  // Create cache workers constructors
+  var deleteCacheExpired = workerFarm(require.resolve('./lib/workers/deleteCacheExpired'));
+  var zsetDecrementer = workerFarm(require.resolve('./lib/workers/zsetDecrementer'));
 
   // Check system dependencies
   // this will modify config.js
@@ -33,5 +38,24 @@ mkdirp('tmp/__queueMsAppIcons', function (err, success) {
 
   app.listen(config.app.http.port, function () {
     console.log('icon-crawler lifted at port ' + config.app.http.port);
+
+    // Start cache workers
+    setInterval(function () {
+      // This worker is responsible to decrement -1 to all
+      // domain in the set
+      zsetDecrementer(function (err) {
+        if (err) {
+          return console.error('Error on zsetDecrementer worker', err);
+        }
+
+        // Now that we decremented all members from the set
+        // time to delete files that are no longer of interest
+        deleteCacheExpired(function (err) {
+          if (err) {
+            return console.error('Error on deleteCacheExpired worker', err);
+          }
+        });
+      });
+    }, parseFloat(config.cache.checkInterval * 60 * 1000));
   });
 });
